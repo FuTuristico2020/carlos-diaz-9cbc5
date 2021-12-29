6a0c753997cf2a0fffc344b36ae064e60ee6af22
Stackbit Code Editor <projects@stackbit.com>
2021-12-29 23:28:59 +0000

    Icon.js: changed

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -47,7 +47,7 @@ export default class Icon extends React.Component {
                 );
             case 'vimeo':
                 return (
-                    <svg width="18px" height="18px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M23.954 4.569a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.691 8.094 4.066 6.13 1.64 3.161a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.2" fill="#FFFFFF"/></svg>
+                    <svg width="18px" height="18px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="" fill="#FFFFFF"/></svg>
                 );
           
             default:
